99e690d4e85378d7ccfa309562974362c815a76d
PogorelovEY <jeckcame@gmail.com>
2021-09-13 16:20:07 +0300

    мелкие правки

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {PersistGate} from "redux-persist/integration/react";
+import {persistStore} from "redux-persist";
 import App from "./App";
 import {ThemeProvider} from '@material-ui/styles';
 import {theme} from "./theme";
 import store from "./store";
-import CssBaseline from '@material-ui/core/CssBaseline';
+
 
 ReactDOM.render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
             <CssBaseline />
             <Provider store={store}>
-                <App/>
+                <PersistGate loading={null} persistor={persistStore(store)}>
+                    <App/>
+                </PersistGate>
             </Provider>
         </ThemeProvider>
     </React.StrictMode>,
--- src/store.js
+++ src/store.js
@@ -1,13 +1,26 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {configureStore, combineReducers} from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
 import chatSlice from "./Chat/chatSlice";
 import profileSlice from "./Profile/profileSlice";
 import thunkMiddleware from "redux-thunk";
+import {persistReducer} from "redux-persist"
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: [], // сюда написать какие редюсеры надо хранить с локальном сторе.
+    blacklist: [],
+};
+
+const reducers = combineReducers({
+    chat: chatSlice,
+    profile: profileSlice,
+})
+
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 
 export default configureStore({
-    reducer: {
-        chat: chatSlice,
-        profile: profileSlice,
-    },
-    middleware: [thunkMiddleware]
+    reducer: persistedReducer,
+    middleware: [thunkMiddleware],
 });
