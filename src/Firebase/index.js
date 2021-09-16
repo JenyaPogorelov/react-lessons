78fe2e294dcfddb29ac46d9b7b67fc2062f330ad
PogorelovEY <jeckcame@gmail.com>
2021-09-16 12:44:46 +0300

    Установил firebase

---
+++ src/Firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA_BWAjxbUtDjuR12oGYH0GnD6WPNMcdtk",
+    authDomain: "react-project-655fe.firebaseapp.com",
+    projectId: "react-project-655fe",
+    storageBucket: "react-project-655fe.appspot.com",
+    messagingSenderId: "8418339955",
+    appId: "1:8418339955:web:5461b66c23e283dacf8e06"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
