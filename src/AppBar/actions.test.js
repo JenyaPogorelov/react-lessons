ebd2883b38bf9927d3a7faa451be5d914183de7d
PogorelovEY <jeckcame@gmail.com>
2021-10-05 15:42:36 +0300

    Сдела простенький тест для редьюсера.

--- src/AppBar/actions.test.js
+++ src/AppBar/actions.test.js
@@ -3,8 +3,10 @@ import {generateChatId} from "./actions";
 describe('Tests', () => {
 
     it ('test reducer', () => {
+        const uidA = 'heY2nmnCCxgkZRUhPkpCdssbOSt2';
+        const uidB = 'DU70o9nFEqTb9cVKc1FBRzn0Tde2';
 
-        expect(generateChatId('heY2nmnCCxgkZRUhPkpCdssbOSt2','DU70o9nFEqTb9cVKc1FBRzn0Tde2')).toEqual('heY2nmnCCxgkZRUhPkpCdssbOSt2-DU70o9nFEqTb9cVKc1FBRzn0Tde2');
+        expect(generateChatId(uidA,uidB)).toEqual('heY2nmnCCxgkZRUhPkpCdssbOSt2-DU70o9nFEqTb9cVKc1FBRzn0Tde2');
 
     })
 
